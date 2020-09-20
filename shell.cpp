7e948429d5a31ee866f5426e1eb495fc06438ce5
arya200 <arya2000@tamu.edu>
2020-09-20 15:38:36 -0400

    everything done with comments

--- shell.cpp
+++ shell.cpp
@@ -9,6 +9,7 @@
 #include <fcntl.h>
 using namespace std;
 
+// trim function reference to Geeks for Geeks
  void trim(string &str) 
 { 
     str += " ";  
@@ -51,12 +52,11 @@ using namespace std;
 vector<string> split(string str, int& pipe_count, char sep, bool& IOper)
 {
     vector<string> bgs;
-    string token;
     string command = "";
 
     for(int i=0;i < str.length(); i++)
     {
-       
+       // checks to see if there is quotes and pipe, then simply adds eeything till delimeter.
         if((str.at(i) == '\"' || str.at(i) == '\'') && pipe_count>1)
         {   
             while(str.at(i) != sep)
@@ -69,7 +69,7 @@ vector<string> split(string str, int& pipe_count, char sep, bool& IOper)
             command="";
             continue;
         }
-        else if((str.at(i) == '\"' || str.at(i) == '\''))
+        else if((str.at(i) == '\"' || str.at(i) == '\'')) //if normal quotes, then keep string between quotes and remove quotes
         {
             i++;
             while(str.at(i)!='\"' && str.at(i) != '\'')
@@ -82,11 +82,11 @@ vector<string> split(string str, int& pipe_count, char sep, bool& IOper)
             continue;
         }
 
-        if(str.at(i) == '<' || str.at(i) == '>')
+        if(str.at(i) == '<' || str.at(i) == '>') // if IO operation
         {
             IOper = true;
             string temp;
-            if(str.at(i-1) != ' ' && str.at(i+1) != ' ')
+            if(str.at(i-1) != ' ' && str.at(i+1) != ' ') //if no space before or after add spaces and the character, and incrememnt
             {
                 temp = command.substr(0, i);
                 temp += " ";
@@ -111,7 +111,7 @@ vector<string> split(string str, int& pipe_count, char sep, bool& IOper)
 
         }
         
-        if(str.at(i) == sep)
+        if(str.at(i) == sep) // if current character  is delimiter, then commmand is full, and can be pushed to the vector
         {
         
             if(command!= " " && command!="")
@@ -126,23 +126,27 @@ vector<string> split(string str, int& pipe_count, char sep, bool& IOper)
         command+= str.at(i);
         
     }
-    if(command!="")
-    {
+    if(command!="") //last command will not be recognized some times as space after it does not exist.  
+    {                // thus if command still have value then push it back to vector.
         bgs.push_back(command);
     }
     
     return bgs;
 }
 
-void find_pipe(string str, int& pipe_count)
+void find_values(string str, int& pipe_count, bool& dir)
 {
-    vector<string> bgs;
-    
+    //checks to see if pipe symbol exists outisde of quotes, and increments pipe count based on number of pipe commands.
+    // also checks to see if there is a "cd" for change directory outside of quotes
     for(int i=0;i < str.length(); i++)
     {
         if(str.at(i) == '|')
         {
             pipe_count++;
+        }
+        if(str.at(i) == 'c' && str.at(i+1) == 'd')
+        {
+            dir = true;
         }
         if((str.at(i) == '\"' || str.at(i) == '\''))
         {
@@ -158,6 +162,7 @@ void find_pipe(string str, int& pipe_count)
   
 }
 
+// reference to Dr. Tanzirs video demo.
 char** vec_to_char_array(vector<string>& parts)
 {
     //cout << "first element in parts is"<<parts[0] <<endl;
@@ -182,34 +187,36 @@ int main ()
     int pipe_count = 1;
     vector<int> bgs;
     char* prev;
-    prev = (char*) malloc( PATH_MAX * sizeof(char) );
-    int backup = dup (0);
-    int backup1 = dup(1);
+    prev = (char*) malloc( PATH_MAX * sizeof(char) ); //pointer to last directory location
+    int backup = dup (0); //back up for standard input.
+    int backup1 = dup(1); //back up for standard output.
     while (true)
     {
         time_t now = time(0);
         char* dt = ctime(&now);
         string time(dt);
-        time = time.substr(0, time.length()-1);
+        time = time.substr(0, time.length()-1); // get current time
         dt = NULL;
-        char *p = getenv("USER");
+        char *p = getenv("USER"); //get user name
         if(p==NULL)
         {
             return EXIT_FAILURE;
         }
+        // set std input and output back to default.
         dup2(backup, 0);
         dup2(backup1, 1);
-        bool IOper = false;
-        bool bg = false;
-        bool dir = false;
-        int pipe_count = 1;
-        string filename = "";
+        bool IOper = false; //if IO operation exists
+        bool bg = false; // if background processes exist
+        bool dir = false; // if change directory function is needed
+        int pipe_count = 1; //number of piping commands.
+        string filename = ""; //filename for IO operation
 
         // check on background processes before getting next;
         for(int k=0; k<bgs.size(); k++)
         {
             if(waitpid(bgs[k], 0, WNOHANG) == bgs[k])
             {
+                //cout is slightely delayed.
                 cout << "Process: " << bgs[k] << " ended" << endl;
                 bgs.erase(bgs.begin() + k);
                 k--;
@@ -227,7 +234,9 @@ int main ()
             prev = NULL;
             break;
         }
-        
+        // check to see if pipe and how many, as well as if change directory
+        find_values(inputline, pipe_count, dir);
+
         //adjust string if background process is needed
         int pos = inputline.find('&');
         if( pos == inputline.length()-1)
@@ -235,9 +244,8 @@ int main ()
             inputline = inputline.substr(0, pos);
             bg = true;
         }      
-
         // if change directory is needed.
-        if(inputline.find("cd") != string::npos)
+        if(inputline.find("cd") != string::npos && dir)
         {  
             //cout << inputline.find("-") << endl;
             if(inputline.find("-") != string::npos)
@@ -286,85 +294,9 @@ int main ()
             continue;
         }
 
-    
-        // vector<string> pparts = split(inputline, '|');
-        // for(int i=0; i<pparts.size(); i++)
-        // {
-            
-        //     int fds[2];
-        //     pipe(fds);
-
-        //     int pid = fork();
-        //     if (pid == 0)
-        //     {   
-        //         //cout <<" I enter here " << endl;
-        //         int pos_write = inputline.find('>');
-        //         int pos_read = inputline.find('<');
-        //         if(pos_write>0)
-        //         {   
-
-        //             string command = "";
-        //             string filename = "";
-        //             command = inputline.substr(0, pos_write);
-        //             trim(command);
-        //             filename = inputline.substr(pos_write + 1);
-        //             trim(filename);
-        //             inputline = command;
-        //             int fd = open(filename.c_str(), O_WRONLY|O_CREAT|O_RDONLY, S_IWUSR| S_IRUSR);
-        //             dup2(fd, 1);
-        //             close(fd);
-        //         }
-        //         if(pos_read>0)
-        //         {   
-
-        //             string command = "";
-        //             string filename = "";
-        //             command = inputline.substr(0, pos_read);
-        //             trim(command);
-        //             filename = inputline.substr(pos_read + 1);
-        //             trim(filename);
-        //             inputline = command;
-        //             int fd = open(filename.c_str(), O_RDONLY, S_IWUSR|S_IRUSR);
-        //             dup2(fd, 0);
-        //             close(fd); 
-        //         }
-        //         if(i < pparts.size() -1)
-        //         {
-        //             dup2(fds[1], 1);
-        //             close(fds[1]);
-            
-        //         }
-
-        //         string command = pparts[i];
-        //         //cout << inputline << endl;
-        //         vector<string> parts = split(command, ' ');
-        //         char** args = vec_to_char_array(parts);
-        //         execvp (args[0], args);
-        //     }
-        //     else
-        //     { 
-        //         if(!bg)
-        //         {
-        //             //cout << "enter here" <<endl;
-        //             waitpid (pid, 0, 0); //parent waits for child process
-        //             dup2(fds[0], 0);
-        //             close(fds[1]);
-        //         }
-        //         else
-        //         {
-        //             bgs.push_back(pid);
-        //         }
-
-            
-        //     }
-        // }
-
-        //pipe_count =2;
-        
-        find_pipe(inputline, pipe_count);
+        // keep a copy of pipe_count
         int temp = pipe_count;
         vector<string> pparts = split(inputline, pipe_count, '|', IOper);
-        //cout << " parts 0 is at line 351: " << parts[0] << endl;
         //if piping operation
         if(pipe_count >1)
         {
@@ -376,9 +308,9 @@ int main ()
                 IOper = false;
                 string curr_command = pparts[i];
                 trim(curr_command);
-                curr_command += " ";
-                pipe_count = 0;
-                
+                curr_command += " "; //space is added, as split needs a space at the end to split accurately
+                pipe_count = 0; // pipe count is made zero so normal quote functionality for strings is processed, and quotes are removed.
+                                // refer to split function to understand more
                 vector<string> commands = split(curr_command, pipe_count, ' ', IOper);
                 if(IOper)
                 {
@@ -386,7 +318,7 @@ int main ()
                     int pos_write = curr_command.find('>');
                     if(pos_write>0)
                     {  
-                        //cout <<"I am in the first if" << endl;
+                        
                         string command = "";
                         string filename = "";
                         command = curr_command.substr(0, pos_write);
@@ -400,7 +332,7 @@ int main ()
                     }
                     if(pos_read>0)
                     {   
-                    //s cout << "I am in the second if " << endl;
+                   
                         string command = "";
                         string filename = "";
                         command = curr_command.substr(0, pos_read);
@@ -439,15 +371,14 @@ int main ()
                 }
                 else
                 {
-                    if(i==pparts.size()-1)
+                    if(i==pparts.size()-1) //only wait for last child.
                     {
                         waitpid(pid, 0,0);
                     }
                     else
-                    {
+                    {  // add pids to background processes vector so as to reap zombie processes of child pipe commands
                         bgs.push_back(pid);
                     }
-                    //bgs.push_back(pid);
                     dup2(fds[0], 0);
                     close(fds[1]);
                 }
@@ -458,11 +389,10 @@ int main ()
            continue;
         }
     
-        
+        // if it is simply a single command it will run this code below
         int pid = fork();
-        //int pid = 1;
         if (pid == 0)
-        {   //cout <<"I make it here" << endl;
+        {
             
            if(IOper)
            {
@@ -470,7 +400,7 @@ int main ()
                 int pos_write = inputline.find('>');
                 if(pos_write>0)
                 {  
-                    //cout <<"I am in the first if" << endl;
+            
                     string command = "";
                     string filename = "";
                     command = inputline.substr(0, pos_write);
@@ -484,7 +414,7 @@ int main ()
                 }
                 if(pos_read>0)
                 {   
-                   //s cout << "I am in the second if " << endl;
+                
                     string command = "";
                     string filename = "";
                     command = inputline.substr(0, pos_read);
@@ -499,10 +429,10 @@ int main ()
                 
            }
             
-            vector<string> parts = split(inputline, pipe_count, ' ', IOper);
-            //cout << "parts is: " << parts[0] << "    " << parts[1] << endl; 
+            vector<string> parts = split(inputline, pipe_count, ' ', IOper); 
             char** args = vec_to_char_array(parts);
-            if(execvp (args[0], args) == -1);
+
+            if(execvp (args[0], args) == -1) //check for execvp, so as to not pass in an incorrect command
             {
                 cout << "invalid command try again" << endl;
                 break;
@@ -512,25 +442,15 @@ int main ()
         { 
             if(!bg)
             {
-                //cout << "enter here" <<endl;
+                
                 waitpid (pid, 0, 0); //parent waits for child process
             }
             else
             {
-                bgs.push_back(pid);
+                bgs.push_back(pid); //background processes are added to vector of PIDS, to be reaped before next command.
             }
 
            
         }
     }
 }
-
-// for(int i=start_pos; i<pparts.size(); i++)
-                // {
-                //     if(pparts[i] == "|")
-                //     {
-                //         start_pos = i +1;
-                //         break;
-                //     } 
-                //     curr_command += pparts[i] + " ";
-                // }
